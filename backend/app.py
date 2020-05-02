d470b807353ebcc9661f39a214a3ba6c883b89f0
Aditya Acharya <aacha002@ucr.edu>
2020-05-02 15:41:13 -0700

    small changes

--- backend/app.py
+++ backend/app.py
@@ -2,10 +2,25 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
 @app.route('/', methods=['GET', 'POST'])
 def main():
     return {"hello": "World"}
+    
+@app.route('/api', methods=['GET', 'POST'])
+def api():
+    return {"hello": "World"}
+
+@app.route('/api/login', methods=['GET','POST'])
+def login():
+
+    return {"message": "Login Success"}
+
+@app.route('/api/signup', methods=['GET','POST'])
+def signup():
+
+    return {"message": "Signup Success"}
+
+ 
 
 
 if __name__ == "__main__":
