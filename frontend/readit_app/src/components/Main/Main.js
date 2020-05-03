977bf142c6c61e79414566cef360a813ce6aa171
Thomas Chin <thomasc246@gmail.com>
2020-05-03 00:19:53 -0700

    Adding fonts

--- frontend/readit_app/src/components/Main/Main.js
+++ frontend/readit_app/src/components/Main/Main.js
@@ -1,39 +1,39 @@
-import React, { Component, Fragment } from 'react';
-import logo from '../assets/taxi-2.svg';
-import title from '../assets/Readit.svg';
-import {Row,Col} from 'react-bootstrap';
-import './Main.css';
-
-
-
-
+import React, { Component, Fragment } from "react";
+import logo from "../assets/taxi-2.svg";
+import title from "../assets/Readit.svg";
+import { Row, Col } from "react-bootstrap";
+import "./Main.css";
 
 class Main extends Component {
-  render(){
+  render() {
     return (
       <div>
-            <Row>
-            <Col className = "leftCol" lg={4}>
-
-            
-            <img src={logo} className="landing__logo"/>
-            
-
-            </Col>
-           <Col className="rightCol" lg={8}>
-            <Row className="logoContainer"><img src={title} className="logo"/></Row>
-           <Row className="loginButtonContainer"><button className="loginButton">Log In</button></Row>
-           <Row className="signupButtonContainer"><button className="signupButton">Sign Up</button></Row>
-            </Col>
+        <Row>
+          <Col className="leftCol" lg={4}>
+            <img src={logo} className="landing__logo" alt="leftside" />
+          </Col>
+          <Col className="rightCol" lg={8}>
+            <Row className="justify-content-center">
+              <Row className="logoContainer justify-content-center">
+                <img src={title} className="logo" alt="readit" />
+              </Row>
+              <Row className="loginButtonContainer justify-content-center">
+                <button className="loginButton">Log In</button>
+              </Row>
+              <Row className="signupButtonContainer justify-content-center">
+                <button className="signupButton">Sign Up</button>
+              </Row>
+              <Row className="whyUsContainer justify-content-center">
+                <button className="whyUsButton">Why us</button>
+              </Row>
             </Row>
-        
+          </Col>
+        </Row>
       </div>
-      
     );
   }
 }
 export default Main;
-
 
 // <div className="landing__logo-container">
 // <div className="col">
